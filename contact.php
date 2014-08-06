8bfcc8e0dd3d23c5e840c0aa4ece7f4b0c2e0a37
fpckalk <fpckalk@gmail.com>
2014-08-06 16:47:51 +0200

    Minor content flow change

--- contact.php
+++ contact.php
@@ -2,7 +2,15 @@
 
 <article class="single">
 	<h1>Stalk me</h1>
-	<p>Come look me up and we'll have something to drink and be best friends and talk about stuff.</p>
+	<div class="left-content">
+		<p>Come look me up and we'll have something to drink and be best friends and talk about stuff.</p>
+		<p>You're also welcome to contact me via one of these cool things:</p>
+		<ul>
+			<li>E-mail: info(at)fpck.nl</li>
+			<li>Google+: <a href="https://plus.google.com/+FrankKalk/">Frank Kalk</a></li>
+			<li></li>
+		</ul>
+	</div>
 
 	<aside>
 		<a target="blank" href="https://www.google.nl/maps/place/Nicolaas+Beetsweg+30/@52.6068594,4.709467,15z/data=!4m2!3m1!1s0x47c5f80bd07b5545:0xbfae4ad6811e8c31?hl=en">
@@ -11,6 +19,5 @@
 		</a>
 	</aside>
 </article>
-
 
 <?php include_once("includes/footer.php") ?>
